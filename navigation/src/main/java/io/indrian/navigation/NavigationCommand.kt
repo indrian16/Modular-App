a84da7b781ad91506014ff366c0ab84e538ff148
INDRIAN <indrian16@outlook.co.id>
2021-02-08 18:05:41 +0800

    remove navigation module

--- navigation/src/main/java/io/indrian/navigation/NavigationCommand.kt
+++
@@ -1,8 +0,0 @@
-package io.indrian.navigation
-
-import androidx.navigation.NavDirections
-
-sealed class NavigationCommand {
-    data class To(val navDirection: NavDirections) : NavigationCommand()
-    object Back : NavigationCommand()
-}
